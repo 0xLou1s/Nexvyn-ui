086b1718ddbc4f7ff69300aea9304cf83965e387
Nexvyn <amangupta.ans@gmail.com>
2026-06-19 00:28:15 +0530

    feat: add hooks, utility functions, and motion token presets

---
+++ hooks/use-click-outside.ts
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
+export function useClickOutside<T extends HTMLElement>(handler: () => void) {
+  const ref = useRef<T>(null)
+
+  useEffect(() => {
+    const onMouseDown = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler()
+      }
+    }
+
+    document.addEventListener('mousedown', onMouseDown)
+    return () => document.removeEventListener('mousedown', onMouseDown)
+  }, [handler])
+
+  return ref
+}
